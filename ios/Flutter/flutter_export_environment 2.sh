8f5d6e73dad418966a06f8e096d6e8a840e9ac7e
KooWihC <ahnchiwook@ajou.ac.kr>
2025-02-24 14:02:16 +0900

    flutter upgrade

---
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/koowihc/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/koowihc/Desktop/sktProject/app_ui/Front-End"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/koowihc/Desktop/sktProject/app_ui/Front-End/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/koowihc/Desktop/sktProject/app_ui/Front-End/.dart_tool/package_config.json"
